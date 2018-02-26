41064d7f3e0db7782e9d7067eeee3c4a10c12ab6
Chris Barker <PythonCHB@gmail.com>
2018-02-25 22:11:07 -0800

    updated script with a bit more structure and added clear all function.

--- src/neo4japp.py
+++ src/neo4japp.py
@@ -29,44 +29,76 @@ logger = logging.getLogger(__name__)
 
 logger.info('Simple app to illustrate Neo4j and relationships')
 
-config = ConfigParser()
-config.read('../.config/config')
 
-username = config["configuration"]["neo4juser"]
-password = config["configuration"]["neo4jpw"]
-
-graphenedb_url = 'bolt://hobby-opmhmhgpkdehgbkejbochpal.dbs.graphenedb.com:24786'
-graphenedb_user = username
-graphenedb_pass = password
-
-print("Using:", username, password)
-
-driver = GraphDatabase.driver(graphenedb_url,
-                              auth=basic_auth(graphenedb_user, graphenedb_pass))
-
-
-def main():
+def setup_db():
     """
-    Create some rel
+    sets up the connection to the neo4j DB
+
+    configuration (username and password) is read from:
+
+    ../.config/config
     """
-    session = driver.session()
 
-    logger.info('Here we add a Person who has the name Bob. Note: care with quotes!')
+    config = ConfigParser()
+    config.read('../.config/config')
 
-    session.run("CREATE (n:Person {name:'Bob'})")
+    graphenedb_user = config["configuration"]["neo4juser"]
+    graphenedb_pass = config["configuration"]["neo4jpw"]
+    # graphenedb_url = 'bolt://hobby-opmhmhgpkdehgbkejbochpal.dbs.graphenedb.com:24786'
+    graphenedb_url = 'bolt://hobby-khhgnhgpkdehgbkeoldljpal.dbs.graphenedb.com:24786'
 
-    logger.info('Now lets see if we can find Bob')
+    driver = GraphDatabase.driver(graphenedb_url,
+                                  auth=basic_auth(graphenedb_user, graphenedb_pass))
 
-    result = session.run("MATCH (n:Person) RETURN n.name AS name")
+    return driver
 
-    logger.info('And print what we find')
 
-    for record in result:
-        print(record["name"])
+def clear_all(driver):
+    """
+    This clears the entire database, so we can start over
 
-    session.close()
+    NOTE: The Docs say about this:
 
-    logger.info('Note - this needs some exception handling!')
+    "This query isn’t for deleting large amounts of data, but is nice
+    when playing around with small example data sets."
+
+    I suppose if you have a really big datbase, you should just throw
+    it away and make a new one.
+    """
+    logger.info("Running clear_all")
+
+    with driver.session() as session:
+        logger.info("Before clearing: all the records")
+        result = session.run("MATCH (n) RETURN n")
+        msg = ("There are these records:" +
+               "\n".join([str(rec) for rec in result]))
+        session.run("MATCH (n) DETACH DELETE n")
+
+
+def test1(driver):
+    """
+    Create some relations
+    """
+    with driver.session() as session:
+        logger.info('Here we add a Person who has the name Bob. Note: care with quotes!')
+
+        session.run("CREATE (n:Person {name:'Bob'})")
+
+        logger.info('Now lets see if we can find Bob')
+
+        result = session.run("MATCH (n:Person) RETURN n.name AS name")
+
+        logger.info('And print what we find')
+
+        for record in result:
+            print(record["name"])
+
+        print(dir(record))
+
+        logger.info('Note - this needs some exception handling!')
+
 
 if __name__ == '__main__':
-    main()
+    driver = setup_db()
+    test1(driver)
+
